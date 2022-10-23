e66ca96193684aff9ac7eab64e616522de4c1149
Samuel Selleck <sellecksamuel@gmail.com>
2022-10-23 13:28:36 +0200

    timings

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use flights_parser::{FlightsParser};
 use airports::{AirportFinder};
 
 use clap::Parser;
+use std::time::Instant;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -26,12 +27,13 @@ fn main() -> io::Result<()> {
 
     let args = Args::parse();
     let data = Path::new(&args.path);
-
     println!("loading airports..");
+    let start = Instant::now();
     let airport_file = data.join("airports.csv");
     let airports = AirportFinder::from_csv(airport_file.as_path());
+    println!("Time: {:?}", start.elapsed());
 
-
+    let start = Instant::now();
     let mut graphs = vec![];
     println!("Parsing Networks...");
     for entry in data.read_dir()? {
@@ -45,14 +47,21 @@ fn main() -> io::Result<()> {
             }
         }
     }
+    println!("Time: {:?}", start.elapsed());
+
     println!("Adding results...");
+    let start = Instant::now();
     let mut graph = network::FlightCountNetwork::new(airports.airport_count());
     for g in graphs {
         graph.add_network(g)
     }
+    println!("Converting...");
     let matrix = graph.connections().iter().map(|x| *x as f32).collect();
+    println!("Time: {:?}", start.elapsed());
     println!("Clustering...");
+    let start = Instant::now();
     let res = clusterer::cluster(matrix, airports.airport_count(), 5);
+    println!("Time: {:?}", start.elapsed());
     println!("{:?}", res);
     Ok(())
 }
