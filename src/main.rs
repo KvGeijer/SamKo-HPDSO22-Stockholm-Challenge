6aa3d29e7d546a52bf9787b1f2b2be4672c12b95
Kåre <kare.kvg@gmail.com>
2022-10-26 20:00:52 +0200

    Remove chaos test from main

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ mod plot;
 use std::{thread, path::{Path, PathBuf}};
 use std::sync::{Mutex, Arc};
 
-use flights_parser::{Flight, FlightsParser};
-use airports::{Airport, AirportFinder, HashAirportFinder, KdTreeAirportFinder};
+use flights_parser::FlightsParser;
+use airports::{Airport, HashAirportFinder};
 
 use clap::Parser;
 use std::time::Instant;
@@ -149,104 +149,4 @@ fn dissimilarity_from_binary(mut dissimilarity_graph: network::FlightCountNetwor
     dissimilarity_graph.add_flights(&flights, airport_finder);
     println!("Locating airports... OK. Time: {:?}", start.elapsed());
     dissimilarity_graph
-}
-
-#[test]
-fn test_airport_finding() {
-    // Compare two methods of testing
-
-    let airports = airports::from_csv("data/airports.csv");
-
-    // Compare different implementations
-    let airport_finder_tree = KdTreeAirportFinder::new(&airports);
-    let airport_finder_hash = HashAirportFinder::new(&airports);
-    let airport_finder_loop = DoubleLoopAirportFinder::new(&airports);
-    let airport_finder = &airport_finder_hash;
-
-    // Only take a 1000 since loop is so incredibly slow
-    let flights:Vec<Flight> = FlightsParser::parse(&Path::new("data/dat.bin"))
-        .into_iter()
-        .take(1000)
-        .collect();
-
-    // Assert all finders give the same results
-    for flight in flights.iter() {
-        let start_hash = airport_finder_hash.closest_ind(flight.from_lat, flight.from_long);
-        let end_hash = airport_finder_hash.closest_ind(flight.to_lat, flight.to_long);
-
-        let start_tree = airport_finder_tree.closest_ind(flight.from_lat, flight.from_long);
-        let end_tree = airport_finder_tree.closest_ind(flight.to_lat, flight.to_long);
-
-        let start_loop = airport_finder_loop.closest_ind(flight.from_lat, flight.from_long);
-        let end_loop = airport_finder_loop.closest_ind(flight.to_lat, flight.to_long);
-
-        assert_eq!(start_hash, start_loop);
-        assert_eq!(start_hash, start_tree);
-        assert_eq!(end_hash, end_loop);
-        assert_eq!(end_hash, end_tree);
-    }
-
-    // Get the upper triangular flight graph
-    let mut dissimilarity_graph = network::FlightCountNetwork::new(airports.len());
-    dissimilarity_graph.add_flights(&flights, airport_finder);
-    let original_upper_triangular = dissimilarity_graph.to_u32_vec();
-
-    // Now get it by not doing upper triangular version
-    // Initialize
-    let mut simple_matrix = vec![];
-    for _ in 0..airports.len() {
-        simple_matrix.push(vec![0; airports.len()]);
-    }
-    // Add all flights symetrically
-    for flight in flights.iter() {
-        let start = airport_finder.closest_ind(flight.from_lat, flight.from_long);
-        let end = airport_finder.closest_ind(flight.to_lat, flight.to_long);
-        if start == end {
-            // Mirror functionality in network
-            continue;
-        }
-
-        simple_matrix[start][end] += 1;
-        simple_matrix[end][start] += 1;
-        if start == end {
-            println!("From and to the same place!? {} & {}", start, end);
-            println!("Lat and long for point: {} {} to {} {}", flight.from_lat, flight.from_long, flight.to_lat, flight.to_long);
-        }
-    }
-    // Convert to upper triangular represented as vector
-    let mut simple_upper_triangular = vec![0; original_upper_triangular.len()];
-    let mut ind = 0;
-    for row in 0..airports.len() {
-        for col in (row + 1)..airports.len() {
-            simple_upper_triangular[ind] = simple_matrix[row][col];
-            ind += 1;
-        }
-    }
-    assert_eq!(original_upper_triangular.len(), ind);
-
-    // They should be equal!
-    assert_eq!(original_upper_triangular.iter().sum::<u32>(), simple_upper_triangular.iter().sum::<u32>());
-
-    let mut sparse_original = vec![];
-    let mut sparse_simple = vec![];
-
-    for (ind, &val) in original_upper_triangular.iter().enumerate() {
-        if val != 0 {
-            sparse_original.push((ind, val));
-        }
-    }
-    for (ind, &val) in simple_upper_triangular.iter().enumerate() {
-        if val != 0 {
-            sparse_simple.push((ind, val));
-        }
-    }
-
-    for (&v1, &v2) in sparse_original.iter().zip(sparse_simple.iter()) {
-        println!("Sparse: {:?} and {:?}", v1, v2);
-        assert_eq!(v1, v2);
-    }
-
-    assert_eq!(sparse_original, sparse_simple);
-    assert_eq!(original_upper_triangular, simple_upper_triangular);
-
 }
